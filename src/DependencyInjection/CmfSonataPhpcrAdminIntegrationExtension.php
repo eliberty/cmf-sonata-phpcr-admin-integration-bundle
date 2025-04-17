8a9f66e0fc905c3a1dfa5eba10a6d24524250bf2
Maxime <maxime.brignon@eliberty.fr>
2025-04-17 10:07:56 +0200

    chore: remove not working service

--- src/DependencyInjection/CmfSonataPhpcrAdminIntegrationExtension.php
+++ src/DependencyInjection/CmfSonataPhpcrAdminIntegrationExtension.php
@@ -60,7 +60,7 @@ class CmfSonataPhpcrAdminIntegrationExtension extends Extension implements Compi
         $this->loadBundles($config['bundles'], $loader, $container);
 
         $loader->load('main.xml');
-        $loader->load('enhancer.xml');
+        // $loader->load('enhancer.xml');
     }
 
     /**
